73a573ae6ec0e09ec338d12c044d1824a969d2eb
Diogo Miranda <damiranda@sga.pucminas.br>
2023-08-19 23:23:05 -0300

    feat: color picker + tool picker

--- image_workspace.py
+++ image_workspace.py
@@ -5,12 +5,15 @@ from tkinter import ttk, colorchooser
 from tkinter import Canvas, messagebox
 from PIL import Image, ImageDraw
     
+from enum import Enum
+   
 class ImageWorkspace:
     """ Mostra a imagem, zoom e filtros"""
 
     def __init__(self, placeholder):
         """ Iniciar a imagem inicial """
-        self.__image_frame = ctk.CTkFrame(placeholder)     # Define um placeholder inicial para renderizar a imagem
+        self.__image_frame = ctk.CTkFrame(placeholder)     # Define um placeholder inicial para renderizar a image
+        
 
         # Definição do workspace da imagem e atribuindo as barras de rolagem
         # Público para alteração por outras classes
@@ -18,11 +21,15 @@ class ImageWorkspace:
                                 highlightthickness=0)
         self.canvas.grid(row=0, column=0, sticky='nswe')
         self.canvas.update()
-        
+
         # Setup variables
-        self.active_tool = "pen"
+        self.tolls = ["Pen"]
+        self.colors = ["black", "blue", "white", "yellow", "brown", "purple", "green"]
+
+        self.active_tool = self.tolls[0]
         self.text_input = None
         self.current_color = "black"
+        self.eraser_color = "white"
 
         # Atribuir botões para trabalhar em conjunto com o workspace
         self.__bind_keys()
@@ -33,16 +40,23 @@ class ImageWorkspace:
         self.canvas.bind("<ButtonRelease-1>", self.stop_drawing)
     
     def start_drawing(self, event):
-        if self.active_tool == "pen":
-            self.last_x = event.x
-            self.last_y = event.y
+        self.last_x = event.x
+        self.last_y = event.y
 
     def draw(self, event):
-        if self.active_tool == "pen":
+        if self.active_tool == "Rubber":
+            x, y = event.x, event.y
+            self.canvas.create_line(self.last_x, self.last_y, x, y, fill=self.eraser_color, width=5, capstyle=tk.ROUND, smooth=tk.TRUE)
+            self.last_x = x
+            self.last_y = y
+        elif self.active_tool == "Pen":
             x, y = event.x, event.y
             self.canvas.create_line(self.last_x, self.last_y, x, y, fill=self.current_color, width=2, capstyle=tk.ROUND, smooth=tk.TRUE)
             self.last_x = x
             self.last_y = y
+
+    def set_active_tool(self, toll):
+        self.active_tool = toll
     
     def stop_drawing(self, event):
         pass
--- main.py
+++ main.py
@@ -12,11 +12,46 @@ class MainWindow(CTkScalingBaseClass):
 
         # Posiciona no centro da tela
         self.canvas.grid(row=0, column=0)
+    
+    def set_active_tool(self):
+        self.canvas.active_tool = self.tolls_optionmenu_var.get()
+    
+    def set_rubber_as_tool(self):
+        self.canvas.active_tool = "Rubber"
+
+    def set_active_tool(self):
+        self.canvas.current_color = self.colors_optionmenu_var.get()
 
     def __load_custom_bar(self):
          # # Cria um frame lateral para a barra de customizações
         self.left_frame = ctk.CTkFrame(root, width=300, height=600)
-        # self.left_frame.configure(width=400)
+
+        self.tolls_optionmenu_var = ctk.StringVar(value="Pen")
+
+        tolls = self.canvas.tolls
+
+        self.tolls_option_menu = ctk.CTkOptionMenu(
+            self.left_frame,
+            values=tolls,
+            variable=self.tolls_optionmenu_var)
+
+        self.tolls_option_menu.grid(row=0, column=0, pady=15, padx=10)
+
+        self.draw_button = ctk.CTkButton(
+            self.left_frame,
+            text="Draw",
+            font=('Sans-serif', 15),
+            command=self.set_active_tool)
+        
+        self.draw_button.grid(row=1, column=0, pady=15, padx=50)
+
+        self.erase_button = ctk.CTkButton(
+            self.left_frame,
+            text="Erase",
+            font=('Sans-serif', 15),
+            command=self.set_rubber_as_tool)
+        
+        self.erase_button.grid(row=2, column=0, pady=15, padx=50)
 
         # Posiciona no centro da tela
         self.left_frame.grid(
@@ -25,6 +60,25 @@ class MainWindow(CTkScalingBaseClass):
             sticky="nsew",
             ipadx=10,
             padx=15)
+        
+        self.colors_optionmenu_var = ctk.StringVar(value="black")
+
+        colors = self.canvas.colors
+
+        self.colors_option_menu = ctk.CTkOptionMenu(
+            self.left_frame,
+            values=colors,
+            variable=self.colors_optionmenu_var)
+
+        self.colors_option_menu.grid(row=3, column=0, pady=15, padx=10)
+
+        self.color_button = ctk.CTkButton(
+            self.left_frame,
+            text="Pick color",
+            font=('Sans-serif', 15),
+            command=self.set_active_tool)
+        
+        self.color_button.grid(row=4, column=0, pady=15, padx=50)
 
     def __init__(self, mainframe):
         # Inicializa o Frame inicial
@@ -45,6 +99,8 @@ class MainWindow(CTkScalingBaseClass):
         # Permite que o quadro inicial se expanda de forma responsiva
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
+
+        self.set_active_tool()
 
 
 root = ctk.CTk()
